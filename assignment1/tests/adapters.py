6c64d704b6efc14579988320915d0287c186aefe
starFalll <fyxformal@gmail.com>
2025-09-21 21:30:26 -0700

    update adapter

--- assignment1/tests/adapters.py
+++ assignment1/tests/adapters.py
@@ -8,6 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
+from cs336_basics import tokenizer
 
 
 def run_linear(
@@ -559,7 +560,8 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return tokenizer.BPETokenizer(vocab, merges, special_tokens)
+    
 
 
 def run_train_bpe(
